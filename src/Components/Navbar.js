baa8cebe7bb50cd95a9ab8820001ef433b84c3c1
ayush23719 <91633771+ayush23719@users.noreply.github.com>
2022-05-28 01:52:32 +0530

    bugs

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,21 +3,30 @@ import { Link } from 'react-router-dom';
 import '../Styles/Navbar.css';
 export default function Navbar(props) {
     const [checked, setChecked] = useState(false);
+    const setOverflow = () => {
+        if (checked === false) {
+            document.body.style.overflow = 'hidden';
+        }
+        else {
+            document.body.style.overflow = 'scroll';
+        }
+    }
     const handleNav = () => {
         if (checked === true) {
             setChecked(false);
         }
     }
+
     return (
         <>
             <header className="logo">Scasto</header>
-            <input type="checkbox" id="toggle" className={`input-toggler`} checked={checked} onChange={(e) => setChecked(e.target.checked)} />
+            <input type="checkbox" id="toggle" className={`input-toggler`} checked={checked} onChange={(e) => setChecked(e.target.checked)} onClick={setOverflow} />
             <label for="toggle" className="menu-toggler">
                 <span className="menu-toggler-line"></span>
                 <span className="menu-toggler-line"></span>
                 <span className="menu-toggler-line"></span>
             </label>
-            <aside className={`sidebar `}>
+            <aside className={`sidebar `} >
                 <ul className="menu">
                     <li><Link className="menu-link" to="/Scasto" onClick={handleNav}>{props.home}</Link></li>
                     <li><Link className="menu-link" to="/Scasto/about" onClick={handleNav}>{props.about}</Link></li>
