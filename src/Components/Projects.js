66d9633f7e3d1320d27ee6b5c5f638d21108f31c
ayush23719 <91633771+ayush23719@users.noreply.github.com>
2022-05-27 23:16:00 +0530

    cards

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,61 +1,75 @@
 import React from 'react'
-import '../Styles/Projects.css'
+import '../Styles/Project.css'
+
 export default function Projects() {
     return (
         <>
-            <header className="myprojects">
-                <h1><span className="prj-1">My</span><span className="prj-2">Projects</span></h1>
-            </header>
-            <div className="cd-container">
-
-                <div className="cd">
-                    <div className="img1"></div>
-                    <div className="cd-title">
-                        Take Quiz
-                    </div>
-                    <div className="cd-desc">
-                        Answer a few questions in a simple quiz format. Let us know how you are feeling. We'll try to bring a
-                        smile on your face.
-                    </div>
-                </div>
-                <div className="cd">
-                    <div className="img2"></div>
-                    <div className="cd-title">
-                        View Results
-                    </div>
-                    <div className="cd-desc">
-                        We measure your mental health and display the results based on your inputs and provide you with videos and
-                        music to enlighten your mood.
-                    </div>
-                </div>
-                <div className="cd">
-                    <div className="img3"></div>
-                    <div className="cd-title">
-                        Track Previous Results
-                    </div>
-                    <div className="cd-desc">
-                        Keep a track of all your previous results. You can save a session as per your choice.
-                    </div>
-                </div>
-                <div className="cd">
-                    <div className="img3"></div>
-                    <div className="cd-title">
-                        Track Previous Results
-                    </div>
-                    <div className="cd-desc">
-                        Keep a track of all your previous results. You can save a session as per your choice.
-                    </div>
-                </div>
-                <div className="cd">
-                    <div className="img3"></div>
-                    <div className="cd-title">
-                        Track Previous Results
-                    </div>
-                    <div className="cd-desc">
-                        Keep a track of all your previous results. You can save a session as per your choice.
-                    </div>
-                </div>
+            <div class="main">
+                <h1>Responsive Card Grid Layout</h1>
+                <ul class="cards">
+                    <li class="cards_item">
+                        <div class="card">
+                            <div class="card_image"><img src="https://picsum.photos/500/300/?image=10" /></div>
+                            <div class="card_content">
+                                <h2 class="card_title">Card Grid Layout</h2>
+                                <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
+                                <button class="btn card_btn">Read More</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="cards_item">
+                        <div class="card">
+                            <div class="card_image"><img src="https://picsum.photos/500/300/?image=5" /></div>
+                            <div class="card_content">
+                                <h2 class="card_title">Card Grid Layout</h2>
+                                <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
+                                <button class="btn card_btn">Read More</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="cards_item">
+                        <div class="card">
+                            <div class="card_image"><img src="https://picsum.photos/500/300/?image=11" /></div>
+                            <div class="card_content">
+                                <h2 class="card_title">Card Grid Layout</h2>
+                                <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
+                                <button class="btn card_btn">Read More</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="cards_item">
+                        <div class="card">
+                            <div class="card_image"><img src="https://picsum.photos/500/300/?image=14" /></div>
+                            <div class="card_content">
+                                <h2 class="card_title">Card Grid Layout</h2>
+                                <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
+                                <button class="btn card_btn">Read More</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="cards_item">
+                        <div class="card">
+                            <div class="card_image"><img src="https://picsum.photos/500/300/?image=17" /></div>
+                            <div class="card_content">
+                                <h2 class="card_title">Card Grid Layout</h2>
+                                <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
+                                <button class="btn card_btn">Read More</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="cards_item">
+                        <div class="card">
+                            <div class="card_image"><img src="https://picsum.photos/500/300/?image=2" /></div>
+                            <div class="card_content">
+                                <h2 class="card_title">Card Grid Layout</h2>
+                                <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
+                                <button class="btn card_btn">Read More</button>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
             </div>
+
         </>
     )
 }
