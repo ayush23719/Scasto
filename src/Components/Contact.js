40da5f60ec3148403ff14ee30324b674abc70206
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-06-24 00:43:03 +0530

    contact-img

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,9 +6,11 @@ export default function Contact() {
       <div className="container-contact">
         <header className="reachmeout">
           <h1><span className="txt-3">Reach</span><span className="txt-4">Me </span><span className='txt-5'>Out</span></h1>
-
+          <span className="contact-text">Want to reach out to me? Sure, why not! </span>
         </header>
-
+        <div className="contact-img">
+          <img src={require('../Assets/contact-img.png')} class="contact-fluid" alt="" />
+        </div>
       </div>
     </>
   )
