739816414456cf065d64abba6430d7dace02d8a8
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-06-24 00:29:29 +0530

    contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import '../Styles/Contact.css'
 export default function Contact() {
   return (
     <>
+      <div className="container-contact">
+        <header className="reachmeout">
+          <h1><span className="txt-3">Reach</span><span className="txt-4">Me </span><span className='txt-5'>Out</span></h1>
+
+        </header>
+
+      </div>
     </>
   )
 }
