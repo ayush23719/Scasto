3d48279e232cdd3aee1bdb9d1f04d3372446f5a5
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-06-24 21:41:35 +0530

    responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -20,22 +20,24 @@ export default function Navbar(props) {
 
     return (
         <>
-            <header className="logo">Scasto</header>
-            <input type="checkbox" id="toggle" className={`input-toggler`} checked={checked} onChange={(e) => setChecked(e.target.checked)} />
-            <label for="toggle" className="menu-toggler">
-                <span className="menu-toggler-line"></span>
-                <span className="menu-toggler-line"></span>
-                <span className="menu-toggler-line"></span>
-            </label>
+            <div className="container-navbar">
+                <header className="logo">Scasto</header>
+                <input type="checkbox" id="toggle" className={`input-toggler`} checked={checked} onChange={(e) => setChecked(e.target.checked)} />
+                <label for="toggle" className="menu-toggler">
+                    <span className="menu-toggler-line"></span>
+                    <span className="menu-toggler-line"></span>
+                    <span className="menu-toggler-line"></span>
+                </label>
 
-            <aside className={`sidebar `} >
-                <ul className="menu">
-                    <li><Link className="menu-link" to="/Scasto" onClick={handleNav}>{props.home}</Link></li>
-                    <li><Link className="menu-link" to="/Scasto/about" onClick={handleNav}>{props.about}</Link></li>
-                    <li><Link className="menu-link" to="/Scasto/projects" onClick={handleNav}>{props.projects}</Link></li>
-                    <li><Link className="menu-link" to="/Scasto/contact" onClick={handleNav}>{props.contact}</Link></li>
-                </ul>
-            </aside>
+                <aside className={`sidebar `} >
+                    <ul className="menu">
+                        <li><Link className="menu-link" to="/Scasto" onClick={handleNav}>{props.home}</Link></li>
+                        <li><Link className="menu-link" to="/Scasto/about" onClick={handleNav}>{props.about}</Link></li>
+                        <li><Link className="menu-link" to="/Scasto/projects" onClick={handleNav}>{props.projects}</Link></li>
+                        <li><Link className="menu-link" to="/Scasto/contact" onClick={handleNav}>{props.contact}</Link></li>
+                    </ul>
+                </aside>
+            </div>
         </>
     )
 }
