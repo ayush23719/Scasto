244c715986c614f12215f182ec1d9e63c862ce5d
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-07-01 20:25:24 +0530

    particlesConfig

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Home from './Components/Home';
 import About from './Components/About';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
-
+import Particles from 'react-particles-js';
+import particlesConfig from './config/particlesConfig';
 import {
   BrowserRouter as Router,
   Routes,
@@ -13,21 +14,26 @@ import {
 function App() {
   return (
     <>
-      <Router>
-        <div className="container-scrollbar">
-          <Navbar home="Home" about="About" projects="Projects" contact="Contact" />
+      <div className="App" style={{ position: 'relative', overflow: "hidden" }}>
+        <div style={{ position: 'absolute' }}>
+          <Particles height="100vh" width="100vw" params={particlesConfig} />
         </div>
-        <Routes>
-          <Route path="/Scasto" element={<Home />}>
-          </Route>
-          <Route path="/Scasto/about" element={<About />}>
-          </Route>
-          <Route path="/Scasto/projects" element={<Projects />}>
-          </Route>
-          <Route path="/Scasto/Contact" element={<Contact />}>
-          </Route>
-        </Routes>
-      </Router>
+        <Router>
+          <div className="container-scrollbar">
+            <Navbar home="Home" about="About" projects="Projects" contact="Contact" />
+          </div>
+          <Routes>
+            <Route path="/Scasto" element={<Home />}>
+            </Route>
+            <Route path="/Scasto/about" element={<About />}>
+            </Route>
+            <Route path="/Scasto/projects" element={<Projects />}>
+            </Route>
+            <Route path="/Scasto/Contact" element={<Contact />}>
+            </Route>
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 }
--- src/config/particlesConfig.js
+++ src/config/particlesConfig.js
@@ -1,11 +1,11 @@
 const particlesConfig = {
-    {
+
     "particles": {
         "number": {
             "value": 200,
-                "density": {
+            "density": {
                 "enable": false,
-                    "value_area": 800
+                "value_area": 800
             }
         },
         "color": {
@@ -13,91 +13,91 @@ const particlesConfig = {
         },
         "shape": {
             "type": "circle",
-                "stroke": {
+            "stroke": {
                 "width": 0,
-                    "color": "#000000"
+                "color": "#000000"
             },
             "polygon": {
                 "nb_sides": 3
             },
             "image": {
                 "src": "img/github.svg",
-                    "width": 100,
-                        "height": 100
+                "width": 100,
+                "height": 100
             }
         },
         "opacity": {
             "value": 0.5,
-                "random": false,
-                    "anim": {
+            "random": false,
+            "anim": {
                 "enable": false,
-                    "speed": 1,
-                        "opacity_min": 0.1,
-                            "sync": false
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
             }
         },
         "size": {
             "value": 3,
-                "random": true,
-                    "anim": {
+            "random": true,
+            "anim": {
                 "enable": false,
-                    "speed": 40,
-                        "size_min": 0.1,
-                            "sync": false
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
             }
         },
         "line_linked": {
             "enable": true,
-                "distance": 150,
-                    "color": "#ffffff",
-                        "opacity": 0.4,
-                            "width": 1
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
         },
         "move": {
             "enable": true,
-                "speed": 6,
-                    "direction": "none",
-                        "random": false,
-                            "straight": false,
-                                "out_mode": "out",
-                                    "bounce": false,
-                                        "attract": {
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
                 "enable": false,
-                    "rotateX": 600,
-                        "rotateY": 1200
+                "rotateX": 600,
+                "rotateY": 1200
             }
         }
     },
     "interactivity": {
         "detect_on": "canvas",
-            "events": {
+        "events": {
             "onhover": {
                 "enable": true,
-                    "mode": "repulse"
+                "mode": "repulse"
             },
             "onclick": {
                 "enable": true,
-                    "mode": "push"
+                "mode": "push"
             },
             "resize": true
         },
         "modes": {
             "grab": {
                 "distance": 400,
-                    "line_linked": {
+                "line_linked": {
                     "opacity": 1
                 }
             },
             "bubble": {
                 "distance": 400,
-                    "size": 40,
-                        "duration": 2,
-                            "opacity": 8,
-                                "speed": 3
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
             },
             "repulse": {
                 "distance": 200,
-                    "duration": 0.4
+                "duration": 0.4
             },
             "push": {
                 "particles_nb": 4
@@ -108,7 +108,7 @@ const particlesConfig = {
         }
     },
     "retina_detect": true
-}
+
 };
 
 export default particlesConfig;
