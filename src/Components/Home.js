18120454a73e8e953d92db8e44729d213b83b9db
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-07-10 12:52:19 +0530

    wavy footer

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@ import '../Styles/Home.css'
 export default function Home() {
     return (
         <>
-            <div className="container">
+            <div className="container-home">
                 <header className="body-text" >
                     <span className="greetings">
                         Hello,
@@ -20,6 +20,34 @@ export default function Home() {
                     <img src={require('../Assets/home-icon.png')} class="img-fluid" alt="" />
                 </div>
             </div>
+            <div>
+                <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                    <defs>
+                        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                    </defs>
+                    <g class="parallax">
+                        <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(114,137,218,0.7)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(114,137,218,0.5)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(114,137,218,0.3)" />
+                        <use xlinkHref="#gentle-wave" x="48" y="7" fill="#7289da" />
+                    </g>
+                </svg>
+            </div>
+
+            <footer class="page-footer  unique-color-dark pt-1">
+                <div class="container">
+                    <ul class="list-unstyled list-inline text-center py-2">
+                        <h5>© 2022 Copyright: Scasto</h5>
+                        {/* <li class="list-inline-item">
+                            <a href="#" class="btn btn-outline-light btn-rounded">yes!</a>
+                        </li><br />
+                        <br /> */}
+                    </ul>
+                </div>
+                {/* <div class="footer-copyright text-center py-3"><h7>© 2020 Copyright:</h7>
+                    <h7 href="https://discord.gg/uRqH5UT"> Croxy</h7>
+                </div> */}
+            </footer>
         </>
     )
 }
