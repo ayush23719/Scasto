8f02997e67d158ed3ed8ec0d3a682419d93c5818
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2023-02-27 23:26:54 +0530

    Contact.js completed

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import '../Styles/Contact.css'
+import React from 'react';
+import '../Styles/Contact.css';
 import { SocialIcon } from 'react-social-icons';
 import Lottie from 'react-lottie';
 import animationData from '../Assets/contact';
-export default function Contact() {
+function Contact() {
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -14,24 +14,35 @@ export default function Contact() {
   };
   return (
     <>
-      <div className="container-contact">
-        <header className="reachmeout">
-          <h1><span className="txt-3">Reach</span><span className="txt-4">Me </span><span className='txt-5'>Out</span></h1>
-          <p className="contact-text">Thanks for Visiting my website!If you liked my work and want to reach out to me, please feel free to do so. You can connect with me on the following platforms.</p>
+      <div className="contact-container">
+        <div className="contact-info">
+          <h1>Contact <span style={{ color: '#ffdf6c', fontWeight: 800 }}>Me</span></h1>
+          <div className="paragraph-container">
+            <p>
+              Thank you for visiting my portfolio! If you have any questions, comments, or inquiries, please don't hesitate to contact me through any of the social media channels listed.
 
-        </header>
-        <div className="socials">
-          <SocialIcon url="https://github.com/ayush23719" />
-          <SocialIcon url="https://www.linkedin.com/in/ayush-pratap-singh-210b38197/" />
-          <SocialIcon url="https://twitter.com/iiush_ps" />
-          <SocialIcon url="discordapp.com/users/728542166313467906" />
-          <SocialIcon url="mailto:ayush23719@gmail.com" />
+              I am always interested in new opportunities and collaborations, so if you have a project in mind, let's discuss how we can work together.
+            </p>
+            <div className="social-icons">
+              <SocialIcon url="https://www.github.com/ayush23719" />
+              <SocialIcon url="https://www.linkedin.com/in/ayush-pratap-singh-210b38197/" />
+              <SocialIcon url="https://discord.com/users/728542166313467906" />
+              <SocialIcon url="https://twitter.com/iiush_ps?t=C5fjfylzVRNfvucNIka6vg&s=09" />
+              <SocialIcon url="mailto:ayush23719@gmail.com" />
+            </div>
+          </div>
         </div>
-        <div className="contact-img">
-          <Lottie options={defaultOptions} />
+        <div className="contact-image">
+          <Lottie
+            options={defaultOptions}
+          />
         </div>
-
       </div>
+      <footer className="footer">
+        Made with ❤️ By Ayush
+      </footer>
     </>
-  )
+  );
 }
+
+export default Contact;
